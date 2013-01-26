75ff85f8118af2700439824a51548c3f07f69ac9
Guilherme Gibertoni <kuramagui@gmail.com>
2013-01-26 11:12:27 -0200

    No more culling
    Now the Ambient Diffuse light is really turned off.
    The player only visualizes the area around him, illuminated by the
    point light (or spot light)

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -11,7 +11,7 @@ public class CameraBehavior : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-		distances = new float[32];
+		/*distances = new float[32];
 		distances[8] = 15.5f;
 		distances[9] = 100;
 		
@@ -27,13 +27,13 @@ public class CameraBehavior : MonoBehaviour
 		camera.cullingMask = (1 << 0) | (1 << 9) | (1 << 8);
 		//camera.cullingMask |= pointLight.cullingMask;
 		
-		mov = transform.parent.GetComponent<PlayerMovement>();
+		mov = transform.parent.GetComponent<PlayerMovement>();*/
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		distances[8] = 15.5f + (1.0f/mov.getDelta())/50f;
-		camera.layerCullDistances = distances;
+		/*distances[8] = 15.5f + (1.0f/mov.getDelta())/50f;
+		camera.layerCullDistances = distances;*/
 	}
 }
