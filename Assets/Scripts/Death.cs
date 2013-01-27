64fda5f973f9d2104ebc58b9128427f79566721f
André Missaglia <andre.missaglia@gmail.com>
2013-01-27 01:03:08 -0200

    Version fix

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -1,12 +1,38 @@
 using UnityEngine;
 using System.Collections;
 
-public class Death : MonoBehaviour {
+public class Death : MonoBehaviour
+{
+	public ParticleSystem deathParticles;
+	private MeshRenderer mesh;
+	public bool isDead = false;
+	public bool alive = true;
+	
+	void AnimateDeath()
+	{
+		deathParticles.Play();
+		
+		mesh = gameObject.GetComponent<MeshRenderer>();
+		mesh.enabled = false;
+		isDead = true;
+	}
 
-	void OnTriggerEnter(Collider objectCollider){
-		if(objectCollider.CompareTag("Player"))
+	void OnTriggerEnter(Collider objectCollider)
+	{
+		/*if(objectCollider.CompareTag("Player"))
 		{
 			Application.LoadLevel(Application.loadedLevel);
+		}*/
+		
+		if(objectCollider.CompareTag("Pith"))
+		{
+			AnimateDeath();
 		}
+	}
+	
+	void Update()
+	{
+		if(isDead == true && !deathParticles.IsAlive())
+			Application.LoadLevel(Application.loadedLevel);
 	}
 }
